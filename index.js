a88c7f89e2f12da34862ceb934b7523f48b6d9f6
Paulo <lider13@gmail.com>
2025-09-09 11:58:25 -0300

    Desafio do Felipao

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let nome= "Salve"
-console.log(nome)
+let heroi= "Capitao JS";
+let xp = 5000;
+
+switch(xp){
+    case 'Bronze' : xp >= 1001 && <= 2000;
+    break;
+    case 'Prata' : xp >= 2001 && <= 5000;
+    break;
+    case 'Ouro' : xp >= 5001 && <= 7000;
+    break;
+    case 'Platina' : xp >= 7001 && <=8000;
+    break;
+    case 'Ascendente' : xp >= 8001 && <= 9000;
+    break;
+    case 'Imortal' : xp >= 9001 && <= 10000;
+    break;
+    case 'Radiante' : xp >= 10001;
+    break;
+    default:
+        console.log ("Nível Ferro até XP 1000");
+        break;
+
+}
+console.log('O herói ${heroi} tem a XP = ${xp}');
